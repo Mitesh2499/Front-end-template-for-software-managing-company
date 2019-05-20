9a2f9927ba8ec28db7c34389f2f4954b46448092
Mitesh2499 <46081187+Mitesh2499@users.noreply.github.com>
2019-05-20 10:27:03 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,10 @@
+//js
+  var typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: [ "Designing.", "Developing.", "Managing.", "More and More...."],
+      typeSpeed: 100,
+      loop: true,
+    });
+  });//js
